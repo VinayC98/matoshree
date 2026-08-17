41b99bfafb569516d5d70c0ee4f8a98e73737ba1
VinayC98 <chavan.r.vinayak@gmail.com>
2026-08-17 11:34:37 +0530

    fix: use environment jwt secret

--- be/src/auth/auth.module.ts
+++ be/src/auth/auth.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthService } from './auth.service.js';
 import { AuthController } from './auth.controller.js';
 import { JwtStrategy } from './jwt.strategy.js';
-
-import { PassportModule } from '@nestjs/passport';
 import { PrismaService } from '../prisma.service.js';
 
 @Module({
   imports: [
     PassportModule,
-    JwtModule.register({
-      secret: 'supersecretkey123',
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '1d',
+        },
+      }),
     }),
   ],
   controllers: [AuthController],
